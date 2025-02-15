15c04bc713851ca4ac8ccc7fd4af595d0989ccc6
Fabian <fabianriveraabian3@gmail.com>
2025-02-15 03:35:27 -0500

    cambio de metadatos y 2da pagina terminada

--- src/app/components/ProjectLetter.tsx
+++ src/app/components/ProjectLetter.tsx
@@ -11,6 +11,11 @@ export default function ProjectLetter() {
   const { id } = useParams();
   const project = projects.find((proy) => proy.title === id);
   if (!project) return <h1>Proyecto no encontrado</h1>;
+  const listR = [
+    { title: "Database", url: project.imgBD, imp: project.imgBdImp, tec: project.tecBd },
+    { title: "Back-end", url: project.imgBE, imp: project.imgBeImp, tec: project.tecBe },
+    { title: "Front-end", url: project.imgFE, imp: project.imgFeImp, tec: project.tecFe },
+  ];
   return (
     <main className="pt-9 pr-[5%] sm:pr-[10%] pb-16 pl-[5%] sm:pl-[10%] 
     text-xl w-full overflow-hidden gap-10 flex flex-col">
@@ -20,8 +25,8 @@ export default function ProjectLetter() {
           {...AnimationUp1}
           className="flex gap-2 items-center flex-col sm:flex-row">
           <h2 className="justify-start w-full sm:w-auto">Technologies: </h2>
-          <div className="flex gap-3">
-            {project.technology.map((list, index)=>(
+          <div className="flex gap-3 flex-wrap">
+            {project.technology.map((list, index) => (
               <div className="bg-quaternary text-primary pr-3 pl-3 rounded-lg text-[14px]" key={index}>{list}</div>
             ))}
           </div>
@@ -33,108 +38,84 @@ export default function ProjectLetter() {
         transition={{ delay: 1, duration: .8 }}
         className="flex gap-4 items-center">
         <h2>Github:</h2>
-        <a href={project.repo} className="text-md text-tertiary hover:text-quaternary">{project.repo}</a>
+        <a href={project.repo} className="text-md text-tertiary hover:text-quaternary truncate">{project.repo}</a>
       </motion.section>
 
       <section className="flex flex-col gap-4">
         <motion.h2 {...AnimationUp2} className="text-3xl font-bold">Project Summary</motion.h2>
         <motion.p {...AnimationUp1} className="text-justify mb-2 text-md">{project.summary}</motion.p>
         <motion.div {...AnimationUp1} className="justify-center w-full flex">
-          <Image src={project.img} alt="imagen de proyecto" className=" w-full h-full md:w-[90%] lg:w-[70%] rounded-lg" />
+          <Image src={project.img} alt="imagen de proyecto" className=" w-full h-full md:w-[90%] lg:w-[70%] rounded-lg" priority/>
         </motion.div>
       </section>
 
-      <section className=" flex flex-col gap-5">
+      <section className=" flex flex-col gap-5 text-justify">
         <motion.h2 {...AnimationUp2} className="text-3xl font-bold">Objective</motion.h2>
-        <motion.p {...AnimationUp1}>acascascaac</motion.p>
-        <ul className="pl-6">
-          <motion.li {...AnimationUp1} className="relative pl-4">
-            <span className="absolute left-0 text-quaternary text-2xl">•</span>
-            adsadadss lols objetivos lalala
-          </motion.li>
-          <motion.li {...AnimationUp1} className="relative pl-4">
-            <span className="absolute left-0 text-quaternary text-2xl">•</span>
-            adsadadss lols objetivos lalala
-          </motion.li>
+        <motion.p {...AnimationUp1}>{project.objP}</motion.p>
+        <ul className="pl-6 flex flex-col gap-5">
+          {project.obj.map((obj, index) => (
+            <motion.li key={index} {...AnimationUp1} className="relative pl-4" >
+              <span className="absolute left-0 text-quaternary text-2xl">•</span>
+              {obj}
+            </motion.li>
+          ))}
         </ul>
       </section>
 
       <section className="flex flex-col gap-4" id="tech">
         <motion.h2 {...AnimationUp2} className="text-3xl font-bold">Technologies Used</motion.h2>
-        <ol>
-          <motion.li {...AnimationUp1} className="flex gap-2">
-            <p>Database:</p><div className="text-3xl"><FontAwesomeIcon icon={faHtml5} /></div>
-          </motion.li>
-          <motion.li {...AnimationUp1} className="flex gap-2">
-            <p>Back-end:</p><div className="text-3xl"><FontAwesomeIcon icon={faHtml5} /></div>
-          </motion.li>
-          <motion.li {...AnimationUp1} className="flex gap-2">
-            <p>Front-end:</p><div className="text-3xl"><FontAwesomeIcon icon={faHtml5} /></div>
-          </motion.li>
+        <ol className="flex flex-col gap-2">
+          {listR.map((list, index) => (
+            <motion.li key={index} {...AnimationUp1} className="flex gap-2 flex-col sm:flex-row">
+              <p>{list.title}:</p>
+              <div className="flex gap-2 flex-wrap">
+                {list.tec.map((tec, index) => (
+                  <div className="bg-quaternary text-primary pr-3 pl-3 rounded-lg text-[14px]" key={index}>{tec}</div>
+                ))}
+              </div>
+            </motion.li>
+          ))}
         </ol>
-        <div className="flex flex-col gap-4">
-          <motion.h3 {...AnimationUp2} className="text-3xl font-bold">Database Design</motion.h3>
-          <motion.div {...AnimationUp1} className="justify-center w-full flex">
-            <Image src={img} alt="imagen de proyecto" className=" w-full h-full md:w-[90%] lg:w-[70%] rounded-lg" />
-          </motion.div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <motion.h3 {...AnimationUp2} className="text-3xl font-bold">Back-end Design:</motion.h3>
-          <motion.div {...AnimationUp1} className="justify-center w-full flex">
-            <Image src={img} alt="imagen de proyecto" className=" w-full h-full md:w-[90%] lg:w-[70%] rounded-lg" />
-          </motion.div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <motion.h3 {...AnimationUp2} className="text-3xl font-bold">Front-end Design:</motion.h3>
-          <motion.div {...AnimationUp1} className="justify-center w-full flex">
-            <Image src={project.imgFE} alt="imagen front-end" className=" w-full h-full md:w-[90%] lg:w-[70%] rounded-lg" />
-          </motion.div>
-        </div>
+        {listR.map((list, index) => (
+          <div className="flex flex-col gap-4" key={index}>
+            <motion.h3 {...AnimationUp2} className="text-3xl font-bold">{list.title} Design:</motion.h3>
+            <motion.div {...AnimationUp1} className="justify-center w-full flex">
+              <Image
+                src={list.url}
+                alt={`img ${list.title}`}
+                className=" w-full h-full md:w-[90%] lg:w-[70%] rounded-lg border-2 border-tertiary" />
+            </motion.div>
+          </div>
+        ))}
       </section>
 
-      <section className="flex flex-col gap-4 " id="stack">
+      <section className="flex flex-col gap-4 text-justify" id="stack">
         <motion.h2 {...AnimationUp2} className="text-3xl font-bold">Implementation</motion.h2>
         <div className="flex flex-col gap-4">
-          <motion.div {...AnimationUp1}>
-            <h3>Database</h3>
-            <ul className="pl-4">
-              <li className="relative pl-4">
-                <span className="absolute left-0 text-quaternary text-2xl">•</span>
-                adsadadss lols objetivos lalala
-              </li>
-            </ul>
-          </motion.div>
-          <motion.div {...AnimationUp1}>
-            <h3>Back-end</h3>
-            <ul className="pl-4">
-              <li className="relative pl-4">
-                <span className="absolute left-0 text-quaternary text-2xl">•</span>
-                adsadadss lols objetivos lalala
-              </li>
-            </ul>
-          </motion.div>
-          <motion.div {...AnimationUp1}>
-            <h3>Front-end</h3>
-            <ul className="pl-4">
-              <li className="relative pl-4">
-                <span className="absolute left-0 text-quaternary text-2xl">•</span>
-                adsadadss lols objetivos lalala
-              </li>
-            </ul>
-          </motion.div>
+          {listR.map((list, index) => (
+            <motion.div key={index} {...AnimationUp1}>
+              <h3>{list.title}</h3>
+              <ul className="pl-4">
+                <li className="relative pl-4">
+                  <span className="absolute left-0 text-quaternary text-2xl">•</span>
+                  {list.imp}
+                </li>
+              </ul>
+            </motion.div>
+          ))}
         </div>
 
         <section id="enlace" className="pb-3 pt-5">
-          <motion.a 
-          {...AnimateOpacity}
-          href={project.repo}
-          className="border-2 border-solid border-quaternary rounded-md pr-4 pl-4 pt-1 pb-1 
+          <motion.a
+            {...AnimateOpacity}
+            href={project.repo}
+            className="border-2 border-solid border-quaternary rounded-md pr-4 pl-4 pt-1 pb-1 
           bg-secondary text-quaternary text-[15px] font-bold items-center flex w-min">
-            Repository <FontAwesomeIcon icon={faGithub} className="ml-2 text-xl"/>
+            Repository <FontAwesomeIcon icon={faGithub} className="ml-2 text-xl" />
           </motion.a>
         </section>
       </section>
-      
+
     </main>
   )
 }
--- src/app/lib/bd.ts
+++ src/app/lib/bd.ts
@@ -1,52 +1,42 @@
 import { faHtml5, faCss3, faJs, faReact, faJava, faPython, faSass, faBootstrap } from '@fortawesome/free-brands-svg-icons';
 export const technology = [
-    {
-        icon: faHtml5,
-        text: "Html",
-    },
-    {
-        icon: faCss3,
-        text: "CSS",
-    },
-    {
-        icon: faJs,
-        text: "JS",
-    },
-    
-    {
-        icon: faSass,
-        text: "Sass",
-    },
-    {
-        icon: faReact,
-        text: "React",
-    },
-    {
-        icon: faJava,
-        text: "Java",
-    },
-    {
-        icon: faPython,
-        text: "Python",
-    },
-    {
-        icon: faBootstrap,
-        text: "BootStrap",
-    },
+    { icon: faHtml5, text: "Html", },
+    { icon: faCss3, text: "CSS", },
+    { icon: faJs, text: "JS", },
+    { icon: faSass, text: "Sass", },
+    { icon: faReact, text: "React", },
+    { icon: faJava, text: "Java", },
+    { icon: faPython, text: "Python", },
+    { icon: faBootstrap, text: "BootStrap", },
 ]
-
 import imgUni from "@/app/assets/usc.webp";
 import imgUniFe from "@/app/assets/uscFE.webp";
-
+import imgUniBE from "@/app/assets/uscBE.webp";
+import imgUniBD from "@/app/assets/uscBD.webp";
 // el despliegue no permite require()
 export const projects = [
     {
         title: "USC",
         technology: ["Android studio", "Sprint Boot", "Sql Server"],
-        img:imgUni,
+        img: imgUni,
         summary: "I developed a mobile application for USC students where they can access their course content, view assignments, and chat with other students. I used Android Studio for frontend development, SQL Server for the database, and Spring Boot for the backend, creating a useful platform to manage their academic life and facilitate communication between them.",
         repo: "https://github.com/Fabian1803/App_Uni",
 
+        objP: "The main objective of this application is to provide USC students with a centralized platform where they can:",
+        obj: [
+            "Students can view their courses and resources provided by teachers.",
+            "The app includes messaging functionality that allows students to interact with each other.",
+            "The app includes an authentication system, and a password recovery functionality is also included in case you forget it.",
+            "Students can check their personal data, the subjects they are enrolled in and other important details."
+        ],
+        tecFe: ["Android Studio","Kotlin", "XML", "Jetpack Compose"],
+        tecBe:["Java", "Spring Boot", "REST API"],
+        tecBd:["SQL Server","JPA", "Hibernate"],
         imgFE: imgUniFe,
+        imgBE: imgUniBE,
+        imgBD: imgUniBD,
+        imgFeImp: "The application was developed in Android Studio using Kotlin as the main language, XML for traditional interface design and Jetpack Compose to create user interfaces in a declarative and fluid way.",
+        imgBeImp: "The backend is developed with Spring Boot, providing a RESTful API that communicates with the SQL Server database. Operations include user authentication, message and material management, and password recovery.",
+        imgBdImp: "The SQL Server database management system stores all relevant information, including student details, messages between them, courses and academic materials.",
     }
 ]
